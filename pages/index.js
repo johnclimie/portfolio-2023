60409b497e9636277d1a449abff052a3d70cdc38
John Climie <johnclimie00@gmail.com>
2023-01-09 15:41:02 -0500

    Styled intro

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,30 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        
+        <section className={styles.intro}>
+          <div className={styles.introWindow}>
+            <div className={styles.windowTop}>
+              <div>
+                <div className={styles.green}></div>
+                <div className={styles.yellow}></div>
+                <div className={styles.red}></div>
+              </div>
+
+              <p>intro.txt</p>
+            </div>
+
+            <div className={styles.introContent}>
+              <h1>John Climie</h1>
+              <h2>Full-Stack Web Developer</h2>
+              <div>
+                <h2>About Me</h2>
+                <h2>My Work</h2>
+                <h2>Contact</h2>
+              </div>
+              <h2>Resume</h2>
+            </div>
+          </div>
+        </section>
       </main>
     </>
   )
