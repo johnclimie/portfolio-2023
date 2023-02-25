97952a7d609bebef3c7ea8b4bb38de90892ade2a
John Climie <johnclimie00@gmail.com>
2023-02-24 19:14:52 -0500

    Created new components

---
+++ components/IntroSection.js
@@ -0,0 +1,28 @@
+import DarkWindowTop from "./DarkWindowTop";
+
+export default function IntroSection() {
+    return (
+        <>
+            <section className={styles.intro}>
+                <div className={styles.introWindow}>
+                    <DarkWindowTop title="intro.txt"/>
+
+                    <div className={styles.introContent}>
+                    <h1 className={styles.name}>John Climie</h1>
+                    <h2 className={styles.careerName}>Full-Stack Web Developer</h2>
+
+                    <hr />
+
+                    <div className={styles.introLinks}>
+                        <h2>About Me</h2>
+                        <h2>My Work</h2>
+                        <h2>Contact</h2>
+                    </div>
+                    <h2 className={styles.resumeLink}>Resume</h2>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
+}
