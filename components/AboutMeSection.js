582723584e82f65c520322b49151c60cae81ede1
John Climie <johnclimie00@gmail.com>
2023-03-03 15:27:42 -0500

    Creating more componenets

---
+++ components/AboutMeSection.js
@@ -0,0 +1,51 @@
+import styles from '../styles/AboutMeSection.module.css'
+import MenuBar from './MenuBar'
+
+export default function AboutMeSection() {
+    return (
+        <>
+            <section className={styles.aboutMe}>
+                <div className={styles.menuBar}>
+                    <div className={styles.menuId}>
+                    <p>JC</p>
+                    <p>About Me</p>
+                    </div>
+
+                    <div className={styles.menuTime}>
+                    <p>Wed Jan 4 2:16 PM</p>
+                    </div>
+                </div>
+                
+                <div className={styles.aboutMeContent}>
+                    <div className={styles.aboutMeGrid}>
+                    <div className={styles.aboutMeTxt}>
+                        <LightWindowTop title={"about-me.txt"}/>
+
+                        <div className={styles.aboutMeTxtContent}>
+                        <div>
+                            <p>👨‍💻 John J Climie</p>
+                            <p>📍 Atlanta, GA</p>
+                            <p>📅 1/4/2023</p>
+                        </div>
+
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.skillsFolder}>
+                        <LightWindowTop title={"about-me.txt"}/>
+
+                        <div className={styles.skillsList}>
+                        {skillData.map(skill => {
+                            return ( 
+                            <Skill imgSrc={skill.src} skill={skill.name} key={skill.name}/>
+                            )
+                        })}
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
---
+++ components/MenuBar.js
@@ -0,0 +1,18 @@
+import styles from '../styles/MenuBar.module.css'
+
+export default function MenuBar() {
+    return (
+        <>
+            <div className={styles.menuBar}>
+                <div className={styles.menuId}>
+                <p>JC</p>
+                <p>About Me</p>
+                </div>
+
+                <div className={styles.menuTime}>
+                <p>Wed Jan 4 2:16 PM</p>
+                </div>
+            </div>
+        </>
+    )
+}
