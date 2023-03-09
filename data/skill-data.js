9b1cfa86f607b82f8fcc51f2ffec31db485fe979
John Climie <johnclimie00@gmail.com>
2023-03-08 19:32:11 -0500

    Testing height of about me divs

--- data/skill-data.js
+++ data/skill-data.js
@@ -7,6 +7,38 @@ export default [
         name: 'MongoDBunq',
         src: '/skill_imgs/mongo.png'
     },
+    {
+        name: 'MongoDBunq2',
+        src: '/skill_imgs/mongo.png'
+    },
+    {
+        name: 'MongoDBunq3',
+        src: '/skill_imgs/mongo.png'
+    },
+    {
+        name: 'MongoDBunq4',
+        src: '/skill_imgs/mongo.png'
+    },
+    {
+        name: 'MongoDBunq5',
+        src: '/skill_imgs/mongo.png'
+    },
+    {
+        name: 'MongoDBunq6',
+        src: '/skill_imgs/mongo.png'
+    },
+    {
+        name: 'MongoDBunq7',
+        src: '/skill_imgs/mongo.png'
+    },
+    {
+        name: 'MongoDBunq8',
+        src: '/skill_imgs/mongo.png'
+    },
+    {
+        name: 'MongoDBunq9',
+        src: '/skill_imgs/mongo.png'
+    },
 
 ]
 
