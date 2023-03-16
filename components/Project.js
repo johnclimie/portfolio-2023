6f2a6f48eb740d038ac59baf2bacedcd61c95b84
John Climie <johnclimie00@gmail.com>
2023-03-16 18:54:29 -0400

    Styling for Project

--- components/Project.js
+++ components/Project.js
@@ -2,7 +2,7 @@ import styles from '../styles/Project.module.css';
 import Image from 'next/image';
 import DarkWindowTop from './DarkWindowTop.js';
 
-export default function Project({ title, imgSrc, desc, stack, demo, src}) {
+export default function Project({ title, imgSrc, desc, stack, demo, src }) {
     return (
         <>
             <div className={styles.projectContainer}>
@@ -10,20 +10,23 @@ export default function Project({ title, imgSrc, desc, stack, demo, src}) {
                 <div className={styles.projectContent}>
                     <Image 
                         priority
-                        src='/skill_imgs/mongo.png'
-                        width="300"
-                        height="300"
+                        src='/project_imgs/projectTest.png'
+                        width="1600"
+                        height="1600"
+                        className={styles.projectImg}
                         alt=""
                     />
-                    <ul>
+                    <ul className={styles.projectInfo}>
                         <li>title: Project 1</li>
                         <li>description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
                         <li>Created with:</li>
+                        <div className={styles.projectStack}>
                             <ul>
                                 <li>Lorem</li>
                                 <li>Ipsum</li>
                                 <li>Dolor</li>
                             </ul>
+                        </div>
                         <li>demo</li>
                         <li>source</li>
                     </ul>
