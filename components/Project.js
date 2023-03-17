dc3808587f5af8b5b747accef0974599f8613dfe
John Climie <johnclimie00@gmail.com>
2023-03-17 13:14:47 -0400

    Filled in component to pass props

--- components/Project.js
+++ components/Project.js
@@ -10,25 +10,30 @@ export default function Project({ title, imgSrc, desc, stack, demo, src }) {
                 <div className={styles.projectContent}>
                     <Image 
                         priority
-                        src='/project_imgs/projectTest.png'
+                        src={imgSrc}
                         width="1600"
                         height="1600"
                         className={styles.projectImg}
                         alt=""
                     />
                     <ul className={styles.projectInfo}>
-                        <li>title: Project 1</li>
-                        <li>description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</li>
+                        <li>title: {title}</li>
+                        <li className={styles.desc}>{desc}</li>
                         <li>Created with:</li>
                         <div className={styles.projectStack}>
                             <ul>
                                 <li>Lorem</li>
                                 <li>Ipsum</li>
                                 <li>Dolor</li>
+                                {stack.map(skill => {
+                                    reutrn (
+                                        <li>{skill}</li>
+                                    )
+                                })}
                             </ul>
                         </div>
-                        <li>demo</li>
-                        <li>source</li>
+                        <li><a href={demo} target="_blank">demo 🚀</a></li>
+                        <li><a href={src} target="_blank">source 📁</a></li>
                     </ul>
                 </div>
             </div>
