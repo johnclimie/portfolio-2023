de2dda90d46bea3b4f8850793032c741ecb3b9d3
John Climie <johnclimie00@gmail.com>
2023-03-23 14:57:38 -0400

    Refactored AboutmeSection

--- components/AboutMeSection.js
+++ components/AboutMeSection.js
@@ -12,30 +12,30 @@ export default function AboutMeSection() {
                 <MenuBar />
                 
                 <div className={styles.aboutMeContent}>
-                    <div className={styles.aboutMeGrid}>
-                        <div className={styles.aboutMeTxt}>
+                    <div className={styles.aboutMeContainer}>
+                        <div className={styles.aboutMeWindow}>
                             <LightWindowTop title={"🗒️ about-me.txt"}/>
 
-                            <div className={styles.aboutMeTxtContent}>
-                            <div>
-                                <p>👨‍💻 John J Climie</p>
-                                <p>📍 Atlanta, GA</p>
-                                <p>📅 1/4/2023</p>
-                            </div>
+                            <div className={styles.aboutMeTxt}>
+                                <div>
+                                    <p>👨‍💻 John J Climie</p>
+                                    <p>📍 Atlanta, GA</p>
+                                    <p>📅 1/4/2023</p>
+                                </div>
 
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                             </div>
                         </div>
 
-                        <div className={styles.skillsFolder}>
+                        <div className={styles.skillsWindow}>
                             <LightWindowTop title={"📁 Skills"}/>
 
                             <div className={styles.skillsList}>
-                            {skillData.map(skill => {
-                                return ( 
-                                    <Skill imgSrc={skill.src} skill={skill.name} key={skill.name}/>
-                                )
-                            })}
+                                {skillData.map(skill => {
+                                    return ( 
+                                        <Skill imgSrc={skill.src} skill={skill.name} key={skill.name}/>
+                                    )
+                                })}
                             </div>
                         </div>
                     </div>
