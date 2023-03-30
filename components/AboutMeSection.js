f5a099540749309924646767d7b73a6d2247436e
John Climie <johnclimie00@gmail.com>
2023-03-30 15:39:10 -0400

    Changing bio

--- components/AboutMeSection.js
+++ components/AboutMeSection.js
@@ -33,7 +33,7 @@ export default function AboutMeSection() {
                                     <p>📅 {getDate()}</p>
                                 </div>
 
-                                <p>My name is John Climie. I am a full-stack web developer located in Atlanta, Georgia. Currently, I lean towards front-end development, however I am expanding my skills in backend development, as I specialize in the MERN stack. I am an IT major at Middle Georgia State University. In April of 2022, I achieved a full-stack web development certificate through Georgia Tech. Currently, I am practicing my skills in my own projects, which can be viewed below. I am currently looking for a career opportunity in my field that will benefit my career. If you would like to contact me with any opportunities, feel free to use the contact form below.</p>
+                                <p>I’m John Climie, a MERN stack web developer, student, and music enjoyer. I create websites and my own personal projects in my freetime. Currently, I mainly focus on front-end development, however I am expanding my skills into back-end development. In April of 2022, I achieved a certificate in full-stack web development from Georgia Tech. I am also currently an IT Student at Middle Georgia State University. </p>
                             </div>
                         </div>
 
