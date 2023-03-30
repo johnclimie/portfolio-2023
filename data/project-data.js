5561a409f5abd836691b773a6401627a2f2cab4b
John Climie <johnclimie00@gmail.com>
2023-03-30 13:52:32 -0400

    Refactoring Quotations in data

--- data/project-data.js
+++ data/project-data.js
@@ -1,26 +1,26 @@
 export default [
     {
-        title: 'Test1',
-        imgSrc: '/project_imgs/projectTest.png',
-        desc: 'Lorem ipsum dolor sit amet. Vel odit quidem qui cupiditate nesciunt ut officia beatae! Ea rerum illum aut aliquam temporibus et perspiciatis nesciunt est veniam voluptas ab fugit similique est iste perferendis! Et dolorum recusandae ex cumque consequatur eos quam ipsam. Non alias nesciunt quo accusamus magni qui eius explicabo sit consectetur vitae qui repellendus illo est enim doloremque ut esse excepturi.',
-        stack: ['Lorem', 'Ipsum', 'Dolor'],
-        demo: 'https://www.google.com/',
-        src: 'https://www.google.com/'
+        title: "Test1",
+        imgSrc: "/project_imgs/projectTest.png",
+        desc: "Lorem ipsum dolor sit amet. Vel odit quidem qui cupiditate nesciunt ut officia beatae! Ea rerum illum aut aliquam temporibus et perspiciatis nesciunt est veniam voluptas ab fugit similique est iste perferendis! Et dolorum recusandae ex cumque consequatur eos quam ipsam. Non alias nesciunt quo accusamus magni qui eius explicabo sit consectetur vitae qui repellendus illo est enim doloremque ut esse excepturi.",
+        stack: ["Lorem", "Ipsum", "Dolor"],
+        demo: "https://www.google.com/",
+        src: "https://www.google.com/"
     },
     {
-        title: 'Test2',
-        imgSrc: '/project_imgs/projectTest.png',
-        desc: 'Lorem ipsum dolor sit amet. Vel odit quidem qui cupiditate nesciunt ut officia beatae! Ea rerum illum aut aliquam temporibus et perspiciatis nesciunt est veniam voluptas ab fugit similique est iste perferendis! Et dolorum recusandae ex cumque consequatur eos quam ipsam. Non alias nesciunt quo accusamus magni qui eius explicabo sit consectetur vitae qui repellendus illo est enim doloremque ut esse excepturi.',
-        stack: ['Lorem', 'Ipsum', 'Dolor'],
-        demo: 'https://www.google.com/',
-        src: 'https://www.google.com/'
+        title: "Test2",
+        imgSrc: "/project_imgs/projectTest.png",
+        desc: "Lorem ipsum dolor sit amet. Vel odit quidem qui cupiditate nesciunt ut officia beatae! Ea rerum illum aut aliquam temporibus et perspiciatis nesciunt est veniam voluptas ab fugit similique est iste perferendis! Et dolorum recusandae ex cumque consequatur eos quam ipsam. Non alias nesciunt quo accusamus magni qui eius explicabo sit consectetur vitae qui repellendus illo est enim doloremque ut esse excepturi.",
+        stack: ["Lorem", "Ipsum", "Dolor"],
+        demo: "https://www.google.com/",
+        src: "https://www.google.com/"
     },
     {
-        title: 'Test3',
-        imgSrc: '/project_imgs/projectTest.png',
-        desc: 'Lorem ipsum dolor sit amet. Vel odit quidem qui cupiditate nesciunt ut officia beatae! Ea rerum illum aut aliquam temporibus et perspiciatis nesciunt est veniam voluptas ab fugit similique est iste perferendis! Et dolorum recusandae ex cumque consequatur eos quam ipsam. Non alias nesciunt quo accusamus magni qui eius explicabo sit consectetur vitae qui repellendus illo est enim doloremque ut esse excepturi.',
-        stack: ['Lorem', 'Ipsum', 'Dolor'],
-        demo: 'https://www.google.com/',
-        src: 'https://www.google.com/'
+        title: "Test3",
+        imgSrc: "/project_imgs/projectTest.png",
+        desc: "Lorem ipsum dolor sit amet. Vel odit quidem qui cupiditate nesciunt ut officia beatae! Ea rerum illum aut aliquam temporibus et perspiciatis nesciunt est veniam voluptas ab fugit similique est iste perferendis! Et dolorum recusandae ex cumque consequatur eos quam ipsam. Non alias nesciunt quo accusamus magni qui eius explicabo sit consectetur vitae qui repellendus illo est enim doloremque ut esse excepturi.",
+        stack: ["Lorem", "Ipsum", "Dolor"],
+        demo: "https://www.google.com/",
+        src: "https://www.google.com/"
     }
 ];
--- data/skill-data.js
+++ data/skill-data.js
@@ -1,42 +1,42 @@
 export default [
     {
-        name: 'MongoDB',
-        src: '/skill_imgs/mongo.png'
+        name: "MongoDB",
+        src: "/skill_imgs/mongo.png"
     },
     {
-        name: 'MongoDBunq',
-        src: '/skill_imgs/mongo.png'
+        name: "MongoDBunq",
+        src: "/skill_imgs/mongo.png"
     },
     {
-        name: 'MongoDBunq2',
-        src: '/skill_imgs/mongo.png'
+        name: "MongoDBunq2",
+        src: "/skill_imgs/mongo.png"
     },
     {
-        name: 'MongoDBunq3',
-        src: '/skill_imgs/mongo.png'
+        name: "MongoDBunq3",
+        src: "/skill_imgs/mongo.png"
     },
     {
-        name: 'MongoDBunq4',
-        src: '/skill_imgs/mongo.png'
+        name: "MongoDBunq4",
+        src: "/skill_imgs/mongo.png"
     },
     {
-        name: 'MongoDBunq5',
-        src: '/skill_imgs/mongo.png'
+        name: "MongoDBunq5",
+        src: "/skill_imgs/mongo.png"
     },
     {
-        name: 'MongoDBunq6',
-        src: '/skill_imgs/mongo.png'
+        name: "MongoDBunq6",
+        src: "/skill_imgs/mongo.png"
     },
     {
-        name: 'MongoDBunq7',
-        src: '/skill_imgs/mongo.png'
+        name: "MongoDBunq7",
+        src: "/skill_imgs/mongo.png"
     },
     {
-        name: 'MongoDBunq8',
-        src: '/skill_imgs/mongo.png'
+        name: "MongoDBunq8",
+        src: "/skill_imgs/mongo.png"
     },
     {
-        name: 'MongoDBunq9',
-        src: '/skill_imgs/mongo.png'
+        name: "MongoDBunq9",
+        src: "/skill_imgs/mongo.png"
     },
 ];
