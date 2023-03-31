6992ce93582b7607f506db6e593fe9ec4e7288b4
John Climie <johnclimie00@gmail.com>
2023-03-31 18:36:19 -0400

    Replacing svgs with pngs

--- data/skill-data.js
+++ data/skill-data.js
@@ -1,38 +1,18 @@
 export default [
     {
-        name: "MongoDB",
-        src: "/skill_imgs/mongo.png"
-    },
-    {
-        name: "Express.js",
-        src: "/skill_imgs/express.png"
-    },
-    {
-        name: "React",
-        src: "/skill_imgs/react.png"
-    },
-    {
-        name: "Node.js",
-        src: "/skill_imgs/Node.png"
-    },
-    {
-        name: "Git",
-        src: "/skill_imgs/git.png"
-    },
-    {
-        name: "Next.js",
-        src: "/skill_imgs/next.png"
-    },
-    {
         name: "HTML",
-        src: "/skill_imgs/html.png"
+        src: "/skills_svgs/html.svg"
     },
     {
         name: "CSS",
-        src: "/skill_imgs/css.png"
+        src: "/skills_svgs/css.svg"
     },
     {
         name: "JavaScript",
-        src: "/skill_imgs/javascript.png"
+        src: "/skills_svgs/javascript.svg"
+    },
+    {
+        name: "React",
+        src: "/skills_svgs/react.svg"
     },
 ];
