41c47f3d2a87fa9cf022db3d27fce3878cfae433
John Climie <johnclimie00@gmail.com>
2023-04-01 11:40:05 -0400

    Filled in skills in sill folder

--- data/skill-data.js
+++ data/skill-data.js
@@ -12,7 +12,51 @@ export default [
         src: "/skills_svgs/javascript.svg"
     },
     {
+        name: "Git",
+        src: "/skills_svgs/git.svg"
+    },
+    {
+        name: "MongoDB",
+        src: "/skills_svgs/mongo.svg"
+    },
+    {
+        name: "Express.js",
+        src: "/skills_svgs/express.svg"
+    },
+    {
         name: "React",
         src: "/skills_svgs/react.svg"
     },
-];
+    {
+        name: "Node.js",
+        src: "/skills_svgs/node.svg"
+    },
+    {
+        name: "Next.js",
+        src: "/skills_svgs/nextjs.svg"
+    },
+    {
+        name: "Bootstrap",
+        src: "/skills_svgs/bootstrap.svg"
+    },
+    {
+        name: "SQL",
+        src: "/skills_svgs/sql.svg"
+    },
+    {
+        name: "NPM",
+        src: "/skills_svgs/npm.svg"
+    },
+    {
+        name: "Vite",
+        src: "/skills_svgs/vite.svg"
+    },
+    {
+        name: "Server-side API",
+        src: "/skills_svgs/ssapi.svg"
+    },
+    {
+        name: "Jest",
+        src: "/skills_svgs/jest.svg"
+    },
+]; 
