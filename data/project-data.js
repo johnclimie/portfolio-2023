e0cb0e97ecb71081d637c2924312a03f0a896eea
John Climie <johnclimie00@gmail.com>
2023-04-03 15:28:01 -0400

    Adding image to fill out data for projects

--- data/project-data.js
+++ data/project-data.js
@@ -8,8 +8,8 @@ export default [
         src: "https://www.google.com/"
     },
     {
-        title: "Test2",
-        imgSrc: "/project_imgs/projectTest.png",
+        title: "Personal Portfolio",
+        imgSrc: "/project_imgs/portfolio.png",
         desc: "Lorem ipsum dolor sit amet. Vel odit quidem qui cupiditate nesciunt ut officia beatae! Ea rerum illum aut aliquam temporibus et perspiciatis nesciunt est veniam voluptas ab fugit similique est iste perferendis! Et dolorum recusandae ex cumque consequatur eos quam ipsam. Non alias nesciunt quo accusamus magni qui eius explicabo sit consectetur vitae qui repellendus illo est enim doloremque ut esse excepturi.",
         stack: ["Lorem", "Ipsum", "Dolor"],
         demo: "https://www.google.com/",
