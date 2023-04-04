77d239fd2b75a0a3a33c8e8e23594d360e0a4e91
John Climie <johnclimie00@gmail.com>
2023-04-04 17:46:27 -0400

    Added images and changed titles

--- data/project-data.js
+++ data/project-data.js
@@ -1,7 +1,7 @@
 export default [
     {
-        title: "Test1",
-        imgSrc: "/project_imgs/projectTest.png",
+        title: "Concert Portfolio",
+        imgSrc: "/project_imgs/concertPortfolio.png",
         desc: "Lorem ipsum dolor sit amet. Vel odit quidem qui cupiditate nesciunt ut officia beatae! Ea rerum illum aut aliquam temporibus et perspiciatis nesciunt est veniam voluptas ab fugit similique est iste perferendis! Et dolorum recusandae ex cumque consequatur eos quam ipsam. Non alias nesciunt quo accusamus magni qui eius explicabo sit consectetur vitae qui repellendus illo est enim doloremque ut esse excepturi.",
         stack: ["Lorem", "Ipsum", "Dolor"],
         demo: "https://www.google.com/",
@@ -9,15 +9,15 @@ export default [
     },
     {
         title: "Personal Portfolio",
-        imgSrc: "/project_imgs/portfolio.png",
+        imgSrc: "/project_imgs/personalPortfolio.png",
         desc: "Lorem ipsum dolor sit amet. Vel odit quidem qui cupiditate nesciunt ut officia beatae! Ea rerum illum aut aliquam temporibus et perspiciatis nesciunt est veniam voluptas ab fugit similique est iste perferendis! Et dolorum recusandae ex cumque consequatur eos quam ipsam. Non alias nesciunt quo accusamus magni qui eius explicabo sit consectetur vitae qui repellendus illo est enim doloremque ut esse excepturi.",
         stack: ["Lorem", "Ipsum", "Dolor"],
         demo: "https://www.google.com/",
         src: "https://www.google.com/"
     },
     {
-        title: "Test3",
-        imgSrc: "/project_imgs/projectTest.png",
+        title: "E-commerce Web Page",
+        imgSrc: "/project_imgs/ecommerceWebPage.png",
         desc: "Lorem ipsum dolor sit amet. Vel odit quidem qui cupiditate nesciunt ut officia beatae! Ea rerum illum aut aliquam temporibus et perspiciatis nesciunt est veniam voluptas ab fugit similique est iste perferendis! Et dolorum recusandae ex cumque consequatur eos quam ipsam. Non alias nesciunt quo accusamus magni qui eius explicabo sit consectetur vitae qui repellendus illo est enim doloremque ut esse excepturi.",
         stack: ["Lorem", "Ipsum", "Dolor"],
         demo: "https://www.google.com/",
